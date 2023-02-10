ae1c9cc76f5997a9e28dba5d6ece5beb15f42e16
Neyvit <successfullproject@gmail.com>
2023-02-10 14:33:13 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/examapp/ExamAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamAppApplication.class, args);
+    }
+
+}
